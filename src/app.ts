a2b94994c8a457433cf2c6cfa8863da0513ff20e
higashiaka <higashiaka0918@gmail.com>
2026-08-13 00:12:47 +0900

    chore: remove unused Timer and Group scaffolding endpoints

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,10 @@ import alertSettingRouter from './domains/alertSetting/interfaces/alertSettingRo
 import authRouter from './domains/auth/interfaces/authController';
 import dashboardRouter from './domains/dashboard/interfaces/dashboardRouter';
 import deviceUsageRouter from './domains/deviceUsage/interfaces/deviceUsageRouter';
-import groupRouter from './domains/group/interfaces/groupRouter';
 import appGoalRouter from './domains/appGoal/interfaces/appGoalRouter';
 import monitoredAppRouter from './domains/monitoredApp/interfaces/monitoredAppRouter';
 import totalGoalRouter from './domains/totalGoal/interfaces/totalGoalRouter';
 import reminderRouter from './domains/reminder/interfaces/reminderRouter';
-import timerRouter from './domains/timer/interfaces/timerRouter';
 import userRouter from './domains/user/interfaces/userRouter';
 import usageLogRouter from './domains/usageLog/interfaces/usageLogRouter';
 import usageReasonRouter from './domains/usageReason/interfaces/usageReasonRouter';
@@ -54,8 +52,6 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api/auth', authRouter);
-app.use('/api/timers', timerRouter);
-app.use('/api/groups', groupRouter);
 app.use('/api/users', userRouter);
 app.use('/api/monitored-apps', monitoredAppRouter);
 app.use('/api/app-goals', appGoalRouter);
